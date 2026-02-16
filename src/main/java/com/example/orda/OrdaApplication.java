c8ff045462dc44f309d34be5df59275fbed42ed8
Ekow Nii Saki Sackey <ekow.sackey@amalitech.com>
2026-02-16 12:51:05 +0000

    init

---
+++ src/main/java/com/example/orda/OrdaApplication.java
@@ -0,0 +1,14 @@
+package com.example.orda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdaApplication.class, args);
+        System.out.println("Let's begin");
+	}
+
+}
---
+++ src/test/java/com/example/orda/OrdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
